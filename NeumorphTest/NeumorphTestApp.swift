3f7f08a0a88bd82bedd794f292d456c699a2c4fc
Anatoly Siliverstov <siliverst0v@yandex.ru>
2022-04-07 00:20:34 +0300

    Initial Commit

---
+++ NeumorphTest/NeumorphTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphTestApp.swift
+//  NeumorphTest
+//
+//  Created by Анатолий Силиверстов on 07.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
