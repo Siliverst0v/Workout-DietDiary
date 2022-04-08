511891c69f549066b2b830abd0d1ef80496cdca6
Anatoly Siliverstov <siliverst0v@yandex.ru>
2022-04-08 23:59:05 +0300

    add image, border of cells

--- NeumorphTest/ContentView.swift
+++ NeumorphTest/ContentView.swift
@@ -34,8 +34,13 @@ struct NeuromorphicRectangleCell: View {
             Button( action: {
             })
             {
+                HStack {
+                    Image("CellChest")
+                        .resizable()
+                        .frame(width: 70, height: 36)
                 Text("Exercise for example to add to Workout Diary App")
                     .fontWeight(.bold)
+                }
             }
             .buttonStyle(UnTappedButtonStyle())
         }
@@ -46,7 +51,6 @@ struct NeuromorphicRectangleCell: View {
 struct UnTappedButtonStyle: ButtonStyle {
     func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
-            .padding(30)
             .font(.system(size: 17))
             .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
             .multilineTextAlignment(.trailing)
@@ -56,11 +60,21 @@ struct UnTappedButtonStyle: ButtonStyle {
                         .fill(.white)
                         .shadow(color: .black.opacity(0.2), radius: 10, x: 5, y: 5)
                         .shadow(color: .white.opacity(0.7), radius: 10, x: -5, y: -5)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 11)
+                                .stroke(Color.gray, lineWidth: 0.1)
+                        )
+                        
                 )
+                .padding(.vertical, 5)
     }
 }
 
-
+struct TappedButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+    }
+}
 
 
 
