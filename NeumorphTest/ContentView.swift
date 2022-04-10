a6beee5cca3e956b292c36503abdd68f283a13c1
Anatoly Siliverstov <siliverst0v@yandex.ru>
2022-04-10 20:09:58 +0300

    2321

--- NeumorphTest/ContentView.swift
+++ NeumorphTest/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
             ScrollView {
             NeuromorphicRectangleCell()
             NeuromorphicRectangleCell()
+                TappedCell()
             NeuromorphicRectangleCell()
             NeuromorphicRectangleCell()
             NeuromorphicRectangleCell()
@@ -25,17 +26,15 @@ struct ContentView: View {
 
 
 struct NeuromorphicRectangleCell: View {
-    @State var notTapped = false
+    @State var notTapped = true
     @State var title = "Exercise for example to add to Workout Diary App"
     @State var image = "CellChest"
     @State var test = ""
-    @State var disable = false
     var body: some View {
         ZStack{
             Button( action: {
                 if notTapped {
                     notTapped.toggle()
-                    disable.toggle()
                 } else {
                     notTapped.toggle()
                 }
@@ -48,6 +47,173 @@ struct NeuromorphicRectangleCell: View {
     }
 }
 
+struct TappedCell: View {
+    @State var image = "CellChest"
+    @State var title = "Exercise for example to add to Workout Diary App"
+    @State var test = ""
+    
+    var body: some View {
+        ZStack(alignment: .topTrailing) {
+        Image("TappedCell")
+            .resizable()
+            .frame(width: 360, height: 270)
+            HStack(alignment: .top, spacing: 0) {
+                VStack(spacing: 5) {
+                    HStack(spacing: 0) {
+                Image(image)
+                    .resizable()
+                    .frame(width: 70, height: 36)
+                Text(title)
+                    .fontWeight(.semibold)
+                    .frame(width: 220)
+                    .lineLimit(3)
+                    .scaledToFill()
+//                    .minimumScaleFactor()
+                    .font(.system(size: 14))
+                    .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                }
+                .padding(.vertical, 10)
+                HStack(alignment: .center) {
+                    Text("Подходы")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(red: 142/255, green: 51/255, blue: 46/255))
+                    Text("Повторения")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(red: 142/255, green: 51/255, blue: 46/255))
+                        .padding(.leading, 20)
+                    Text("Вес(Кг)")
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(red: 142/255, green: 51/255, blue: 46/255))
+                }
+                    HStack(alignment: .center) {
+                    Text("1")
+                        .font(.system(size: 17))
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                        .padding(.horizontal, 50)
+                    TextField("0", text: $test)
+                        .frame(width: 60, alignment: .center)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                        .padding(.leading, 33)
+                    TextField("0", text: $test)
+                        .frame(width: 60, alignment: .center)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                        .padding(.horizontal)
+                }
+                HStack(alignment: .center) {
+                    Text("2")
+                        .font(.system(size: 17))
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                        .padding(.horizontal, 50)
+                    TextField("0", text: $test)
+                        .frame(width: 60, alignment: .center)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                        .padding(.leading, 30)
+                    TextField("0", text: $test)
+                        .frame(width: 60, alignment: .center)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                        .padding(.horizontal)
+                }
+                HStack(alignment: .center) {
+                    Text("3")
+                        .font(.system(size: 17))
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                        .padding(.horizontal, 50)
+                    TextField("0", text: $test)
+                        .frame(width: 60, alignment: .center)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                        .padding(.leading, 30)
+                    TextField("0", text: $test)
+                        .frame(width: 60, alignment: .center)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                        .padding(.horizontal)
+                }
+                Button(action: {  }) {
+                    Text("+")
+                        .font(.system(size: 25))
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                }
+                .font(.system(size: 17))
+                .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                .frame(width: 50, height: 28, alignment: .init(horizontal: .center, vertical: .bottom) )
+                .background(
+                            RoundedRectangle(cornerRadius: 11)
+                                    .fill(.white)
+                            .shadow(color: .black.opacity(0.2), radius: 10, x: 5, y: 5)
+                            .shadow(color: .white.opacity(0.7), radius: 10, x: -5, y: -5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 11)
+                                    .stroke(Color.gray, lineWidth: 0.1)
+                            )
+                    )
+                .padding(.trailing,200)
+            }
+                VStack(alignment: .center, spacing: 33) {
+                Button(action: {}) {
+                    Image(systemName: "checkmark")
+                }
+                .frame(width: 40, height: 40, alignment: .center )
+                .font(.system(size: 20))
+                .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                .background(
+                            RoundedRectangle(cornerRadius: 11)
+                            .fill(.white)
+                            .shadow(color: .black.opacity(0.2), radius: 10, x: 5, y: 5)
+                            .shadow(color: .white.opacity(0.7), radius: 10, x: -5, y: -5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 11)
+                                    .stroke(Color.gray, lineWidth: 0.1)
+                            )
+                    )
+                Button(action: {}) {
+                        Image(systemName: "memories")
+                        .padding(.trailing, 1)
+                }
+                .frame(width: 30, height: 105, alignment: .center )
+                .font(.system(size: 17))
+                .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
+                .background(
+                            RoundedRectangle(cornerRadius: 11)
+                            .fill(.white)
+                            .shadow(color: .black.opacity(0.2), radius: 10, x: 5, y: 5)
+                            .shadow(color: .white.opacity(0.7), radius: 10, x: -5, y: -5)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 11)
+                                    .stroke(Color.gray, lineWidth: 0.1)
+                            )
+                    )
+                Button(action: {}) {
+                        Image(systemName: "arrowtriangle.up.fill")
+                    }
+                    .frame(width: 40, height: 40, alignment: .center )
+                    .font(.system(size: 20))
+                    .foregroundColor(Color(red: 142/255, green: 51/255, blue: 46/255))
+                    .background(
+                                RoundedRectangle(cornerRadius: 11)
+                                .fill(.white)
+                                .shadow(color: .black.opacity(0.2), radius: 10, x: 5, y: 5)
+                                .shadow(color: .white.opacity(0.7), radius: 10, x: -5, y: -5)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 11)
+                                        .stroke(Color.gray, lineWidth: 0.1)
+                                )
+                        )
+                }
+                .padding(.trailing, 10)
+                .padding(.top, 10)
+            }
+        }
+    }
+}
 
 struct ButtonContent: View {
     
@@ -58,19 +224,20 @@ struct ButtonContent: View {
     
     var body: some View {
         if tapValue {
-        HStack {
+            HStack(alignment: .center) {
             Image(image)
                 .resizable()
                 .frame(width: 70, height: 36)
-                .padding(.leading)
             Text(title)
             .fontWeight(.semibold)
-            .padding(.trailing)
+            .lineLimit(3)
+            .frame(width: 220)
+            .padding(.trailing, 2)
             Button(action: {}) {
                 Image(systemName: "checkmark")
                     .font(.system(size: 20))
                     .frame(width: 40, height: 40, alignment: .center)
-                    .padding(.trailing, 8)
+                    .padding(.trailing, 10)
             }
         }
         } else {
@@ -254,10 +421,10 @@ struct CustomButtonStyle: ButtonStyle {
         configuration.label
             .font(.system(size: 14))
             .foregroundColor(Color(red: 46/255, green: 74/255, blue: 142/255))
-            .frame(width: 360, height: 50)
+            .frame(width: 360, height: 70, alignment: .trailing)
             .background(
                         RoundedRectangle(cornerRadius: 11)
-                                .fill(.white)
+                        .fill(.white)
                         .shadow(color: .black.opacity(0.2), radius: 10, x: 5, y: 5)
                         .shadow(color: .white.opacity(0.7), radius: 10, x: -5, y: -5)
                         .overlay(
